416be312de34008b1709ba1c2a19ef602bb6a125
AayushAgnihotri <56027538+AayushAgnihotri@users.noreply.github.com>
2025-01-07 00:00:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files for purge
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
